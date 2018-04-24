6bf797b39e8bbed26444090f02343eaf61f5c881
Pletcher <mwpletch@ads.iu.edu>
2018-04-24 13:09:41 -0400

    Add project files.

---
+++ FloodIt/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FloodIt.Startup))]
+namespace FloodIt
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
